46bf3d10a9589837642d98be20dd1181857242ed
gobindatechwens <gobinda.saha@techwens.in>
2026-06-26 09:45:52 +0530

    done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,20 @@
 import express from "express"
 import "dotenv/config"
+import User from "./models/user.model.js";
+import { connectDB } from "./lib/db.js";
+import { clerkMiddleware } from '@clerk/express'
 
 const app=express();
 const PORT=process.env.PORT
+app.use(express.json())
+app.use(cors({origin:FRONTEND_URL,credentials:true}))
+app.use(clerkMiddleware())
 
-console.log(process.env.DB_URL)
-app.listen(3000,()=>console.log("Server is running on port:", PORT));
+app.get("/health",(req,res)=>{
+  res.status(200).json({ok:true})
+});
+
+// console.log(process.env.DB_URL)
+app.listen(PORT,()=>{
+  connectDB();
+  console.log("Server is running on port:", PORT)});
