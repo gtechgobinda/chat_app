bde8a8c76162f4b5cf935683cc97b756dd60030b
gobindatechwens <gobinda.saha@techwens.in>
2026-06-26 14:57:27 +0530

    Add Clerk webhook handling and update environment variables

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,11 +7,14 @@ import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
 import { clerkMiddleware } from '@clerk/express'
 import job from "./lib/cron.js";
+import clerkWebhook from "./webhooks/clerk.webhook.js"
 
 const app=express();
 const PORT=process.env.PORT
 const FRONTEND_URL=process.env.FRONTEND_URL;
 const publicDir=path.join(process.cwd(),"public");
+
+app.use("app/webhooks/clerk",express.raw({type:"application/json"}),clerkWebhook)
 
 
 app.use(express.json())
